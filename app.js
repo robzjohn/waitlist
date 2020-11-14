396d085eb7540c465cfd7f4eae65773018a6b527
rob <robert.z.j64@gmail.com>
2020-11-14 12:57:43 -0500

    cleaned up html and buttons

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const nameInput = document.querySelector(".name-input");
 const phoneInput = document.querySelector(".phone-input");
 const inOut = document.getElementById("in-or-out");
 const partySize = document.getElementById("party-size");
-
-
 const submitButton = document.querySelector(".submit-btn");
 const waitQ = document.querySelector(".wait-queue");
 
@@ -14,21 +12,40 @@ const waitStorage = window.localStorage;
 //Event Listeners
 submitButton.addEventListener("click", addToWait);
 waitQ.addEventListener("click", removeCustomer);
+waitQ.addEventListener("click", sendText);
 window.addEventListener("load", loadWait);
 
 
 function loadWait(){
     console.log('loaded');
+    console.log(waitStorage);
+    for(let name in waitStorage){
+        if(typeof(waitStorage[name]) == 'string'){
+            
+            addCustomer(JSON.parse(waitStorage[name]));
+        }
+    }
 
 
 }
 
+
 function addToWait(event){
 
-    var d = new Date();
     var customer = { "name": nameInput.value, "size": partySize.value, "phone": phoneInput.value, "where": inOut.value }
 
     waitStorage.setItem(customer.name, JSON.stringify(customer));
+
+    addCustomer(customer);
+
+    nameInput.value = "";
+    phoneInput.value = "";
+
+}
+
+function addCustomer(c){
+
+    var d = new Date();
 
     //event.preventDefault();
     const waitQDiv = document.createElement("div");
@@ -36,7 +53,7 @@ function addToWait(event){
 
     //Create li
     const newCustomer = document.createElement("li");
-    newCustomer.innerHTML = nameInput.value.bold() +":&nbsp;" + "<p>party of " + partySize.value + " for " + inOut.value + "<br>phone: " + phoneInput.value + " <br>added  at: " + d.toLocaleTimeString();
+    newCustomer.innerHTML = c.name +":&nbsp;" + "<p>party of " + c.size + " for " + c.where + "<br>phone: " + c.phone + " <br>added  at: " + d.toLocaleTimeString();
     newCustomer.classList.add('customer');
     waitQDiv.appendChild(newCustomer);
 
@@ -54,20 +71,31 @@ function addToWait(event){
 
     waitQ.appendChild(waitQDiv);
 
-    nameInput.value = "";
-    phoneInput.value = "";
-
+    
 }
 
 function removeCustomer(event){
-    const item = event.target;
+    let item = event.target;
 
     if(item.classList[0] === "remove-btn"){
-        const c = item.parentElement;
+        let c = item.parentElement;
         c.remove();
         waitStorage.removeItem(extractName(item.parentElement.innerHTML));
     }
 
+}
+
+function sendText(event){
+    let item = event.target;
+
+    if(item.classList[0] === 'send-btn'){
+        item.classList.add('sent');
+        console.log(item.innerHTML = 'message sent');
+        console.log(JSON.parse(waitStorage.getItem(extractName(item.parentElement.innerHTML))).phone);
+
+        
+
+    }
 }
 
 function extractName(str){
